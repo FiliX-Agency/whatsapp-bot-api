4075829c38a7690d41f9dc63c92ddae5b6ce1614
MohammadReza Shekohi <mrshekohi079@gmail.com>
2025-08-03 22:30:03 +0330

    fix: refined the userSession so it ensures opened connection of the sock to whatsapp

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,16 @@
 import app from "./app.js";
 import { createServer } from "http";
 import { connectToDB } from "./db/index.js";
-import { initUserSession } from "./wa/userSession.js";
-import { waitUntilConnected } from "./wa/waitUntilConnected.js";
+// import { initUserSession } from "./wa/userSession.js";
 
 async function bootstrap() {
   // await startBot();
   await connectToDB();
-  const sock = await initUserSession("688f4a545c7669a3324a887f");
-  await waitUntilConnected(sock!);
-  await sock?.sendMessage("989226734877@s.whatsapp.net", {
-    text: "i see trees of green, red roses too, i see them bloom, for me and you, and i think to myself, WHAT A WONDERFUL WORLD!",
-  });
+  // const sock = await initUserSession("688f4a545c7669a3324a887f");
+  // // await waitUntilConnected(sock!);
+  // await sock?.sendMessage("989226734877@s.whatsapp.net", {
+  //   text: "i see trees of green, red roses too, i see them bloom, for me and you, and i think to myself, WHAT A WONDERFUL WORLD!",
+  // });
   const server = createServer(app);
 
   const port = 4000;
--- src/wa/userSession.ts
+++ src/wa/userSession.ts
@@ -26,59 +26,83 @@ export async function initUserSession(userId: string) {
 
   sock.ev.on("creds.update", saveCreds);
 
-  sock.ev.on(
-    "connection.update",
-    async ({ connection, lastDisconnect, qr }) => {
-      if (qr) {
-        const qrImage = await QRCode.toDataURL(qr);
+  // sock.ev.on(
+  //   "connection.update",
+  //   async ({ connection, lastDisconnect, qr }) => {
+  //     if (qr) {
+  //       const qrImage = await QRCode.toDataURL(qr);
 
-        const base64Data = qrImage.replace(/^data:image\/png;base64,/, "");
-        const buffer = Buffer.from(base64Data, "base64");
-        const qrCodePath = path.join(
-          dirname,
-          `../../private/qr-codes/${crypto.randomUUID()}.png`
-        );
+  //       const base64Data = qrImage.replace(/^data:image\/png;base64,/, "");
+  //       const buffer = Buffer.from(base64Data, "base64");
+  //       const qrCodePath = path.join(
+  //         dirname,
+  //         `../../private/qr-codes/${crypto.randomUUID()}.png`
+  //       );
 
-        if (!fs.existsSync(path.dirname(qrCodePath))) {
-          fs.mkdirSync(path.dirname(qrCodePath), { recursive: true });
-        }
+  //       if (!fs.existsSync(path.dirname(qrCodePath))) {
+  //         fs.mkdirSync(path.dirname(qrCodePath), { recursive: true });
+  //       }
 
-        fs.writeFileSync(qrCodePath, buffer);
+  //       fs.writeFileSync(qrCodePath, buffer);
 
-        const relativeQRCodePath = path.relative(
-          path.join(dirname, "../../"),
-          qrCodePath
-        );
+  //       const relativeQRCodePath = path.relative(
+  //         path.join(dirname, "../../"),
+  //         qrCodePath
+  //       );
 
-        await WASession.updateOne(
-          { userId },
-          { $set: { "qr.url": relativeQRCodePath } },
-          { upsert: true }
-        );
+  //       await WASession.updateOne(
+  //         { userId },
+  //         { $set: { "qr.url": relativeQRCodePath } },
+  //         { upsert: true }
+  //       );
+  //     }
+  //     if (connection === "close") {
+  //       const shouldReconnect =
+  //         (lastDisconnect?.error as any)?.output?.statusCode !==
+  //         DisconnectReason.loggedOut;
+
+  //       if (shouldReconnect) {
+  //         console.log("reconnecting.");
+  //         await initUserSession(userId);
+  //       } else {
+  //         activeSockets.delete(userId);
+  //       }
+  //     } else if (connection === "open") {
+  //       const session = await WASession.findOne({ userId });
+  //       if (session?.qr?.url) {
+  //         const qrCodePath = path.join(dirname, session.qr.url);
+
+  //         fs.unlinkSync(qrCodePath);
+  //       }
+  //     }
+  //   }
+  // );
+
+  const isConnected = new Promise<WASocket>((resolve, reject) => {
+    let isResolved = false;
+
+    sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
+      if (connection === "open" && !isResolved) {
+        isResolved = true;
+        activeSockets.set(userId, sock);
+        resolve(sock);
       }
-      if (connection === "close") {
-        const shouldReconnect =
-          (lastDisconnect?.error as any)?.output?.statusCode !==
-          DisconnectReason.loggedOut;
 
-        if (shouldReconnect) {
-          console.log("reconnecting.");
-          await initUserSession(userId);
+      if (connection === "close" && !isResolved) {
+        const reason = (lastDisconnect?.error as any)?.output?.statusCode;
+        if (reason !== DisconnectReason.loggedOut) {
+          console.log("Trying reconnect...");
+          initUserSession(userId).then(resolve).catch(reject);
         } else {
-          activeSockets.delete(userId);
-        }
-      } else if (connection === "open") {
-        const session = await WASession.findOne({ userId });
-        if (session?.qr?.url) {
-          const qrCodePath = path.join(dirname, session.qr.url);
-
-          fs.unlinkSync(qrCodePath);
+          reject(new Error("Session logged out"));
         }
       }
-    }
-  );
+    });
+  });
 
-  if (activeSockets.has(userId)) return activeSockets.get(userId);
-  activeSockets.set(userId, sock);
-  return sock;
+  return isConnected;
+
+  // if (activeSockets.has(userId)) return activeSockets.get(userId);
+  // activeSockets.set(userId, sock);
+  // return sock;
 }
